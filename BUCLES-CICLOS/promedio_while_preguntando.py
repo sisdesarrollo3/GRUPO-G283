8c1925b6c38a0c5633452109d25ef30f9b4edf56
John Jairo Orozco D. <jorozco@ucm.edu.co>
2025-07-14 19:57:52 -0500

    bucles2

--- BUCLES-CICLOS/promedio_while_preguntando.py
+++ BUCLES-CICLOS/promedio_while_preguntando.py
@@ -12,13 +12,13 @@ suma = 0
 contador = 0
 respuesta = 's'   #variable de control respuesta inicia en s para ingrear por primera vez
 while respuesta  == 's': 
-    numero = int(input("INGRESE NUMERO (NEGATIVO PARA SALIR): "))
+    numero = int(input("INGRESE NUMERO POSITIVO - NEGATIVOS NO SE SUMARAN: "))
     if numero >= 0:
         suma = suma + numero
         contador += 1
     
     #variable de control debe ser actualizada antes de volver al bucle
-    respuesta = input("CONTINUAR (Si/No):")[0].lower()
+    respuesta = input("DESEA CONTINUAR (Si/No):")[0].lower()
 
 #salida tiempo total
 promedio = suma / contador
