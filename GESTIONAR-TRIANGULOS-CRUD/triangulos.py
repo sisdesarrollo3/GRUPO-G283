e30bbcb7519da42ba6cf551904ce3d7effbac812
John Jairo Orozco D. <jorozco@ucm.edu.co>
2025-07-17 21:30:49 -0500

    crud-insertar

--- GESTIONAR-TRIANGULOS-CRUD/triangulos.py
+++ GESTIONAR-TRIANGULOS-CRUD/triangulos.py
@@ -1,9 +1,30 @@
 #importar librerias de python
 import os
 import time
+import sys
 
+# Obtener la ruta absoluta de la carpeta raíz del proyecto (PROYECTO) 
+base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+# Agregar la carpeta BIBLIOTECA al path
+sys.path.append(os.path.join(base_dir, "BIBLIOTECA"))
+# Importar libreria
+import libreria   #la primera vez muestra error de sintaxis
 
 #FUNCIONES PROPIAS
+def leerDatos():
+    valores = {}
+    lado1 = libreria.leerFlotante("LADO 1: ", 1, 9999999999)
+    lado2 = libreria.leerFlotante("LADO 1: ", 1, 9999999999)
+    lado3 = libreria.leerFlotante("LADO 1: ", 1, 9999999999)
+
+    #armar la estructura como diccionario
+    valores = {
+        "lado1": lado1,
+        "lado2": lado2,
+        "lado3": lado3
+    }
+    return valores
+
 def menu ():
     os.system("cls")
     print ("*** GESTIONAR LA ENTIDAD *** ")
@@ -14,18 +35,34 @@ def menu ():
     print ("[5].  ELIMINAR")
     print ("[6].  SALIR")
 
+#INICIO DEL PROGRAMA
 
+#VARIABLES GLOBALES 
+triangulo = {}    #para la estructura individual
+triangulos = {}   #para almacenar todas las estructura individuales
+
+nombreArchivo = "triangulos.dat"
 
 def main():
     while True:
         menu()
-        opcion = input("OPCION: ")[0]
+        opcion = libreria.leerCaracter("OPCION: ")
         match opcion:
             case '1':
-                print("PROXIMAMENTE INSERTAR UNA NUEVA ENTIDAD")
-                time.sleep(1)
+                libreria.limpiarPantalla()
+                print("*** INSERTAR ENTIDAD ***")
+                codigo = libreria.leerCadena("CODIGO: ", 10).lower()
+                if not (codigo in triangulos.keys()):
+                    valores = leerDatos()
+                    triangulos[codigo] = valores
+                    libreria.guardar(triangulos, nombreArchivo)
+                    libreria.mensajeErrorEsperaSegundos("INSERTADO CORRECTAMENTE", 2)
+                else:
+                    libreria.mensajeErrorEsperaSegundos(" CODIGO YA EXISTE - NO PERMITO DUPLICADOS", 2)
             case '2':
-                print("PROXIMAMENTE LISTAR TODOS LOS REGISTROS DE UNA ENTIDAD")
+                libreria.limpiarPantalla()
+                print("*** LISTAR ENTIDAD ***")
+                print(triangulos)
                 input()
                 #time.sleep(1)
             case '3':
@@ -40,7 +77,9 @@ def main():
             case '6':
                 print("SALE DEL PROGRAMA")
                 time.sleep(1)
+                break
 
 
 if __name__ == "__main__":
+    triangulos = libreria.cargar(triangulos, nombreArchivo)    
     main()
